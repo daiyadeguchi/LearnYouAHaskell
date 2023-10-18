ecc151b776269abf773cc066988f9bc525a7b518
daiyadeguchi <double.d.rugby@gmail.com>
2023-10-18 12:00:14 +0900

    learnin functions

--- baby.hs
+++ baby.hs
@@ -5,15 +5,79 @@ doubleSmallNumber x = if x > 100
                       else x*2
 
 boomBangs xs = [ if x < 10 then "BOOM!" else "BANG" | x <- xs, odd x]
-length' xs = sum [1 | _ <- xs]
+--length' xs = sum [1 | _ <- xs]
 removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
 
 addThree :: Int -> Int -> Int -> Int
 addThree x y z = x + y + z
 
-factorial :: Integer -> Integer
-factorial n = product [1..n]
+--factorial :: Integer -> Integer
+--factorial n = product [1..n]
 
 circumference :: Float -> Float
 circumference r = 2 * pi * r
+
+lucky :: (Integral a) => a -> String
+lucky 7 = "LUCKY NUMBER SEVEN!"
+lucky x = "Sorry, you're out of luck, pal!"
+
+sayMe :: (Integral a) => a -> String
+sayMe 1 = "One"
+sayMe 2 = "Two"
+sayMe 3 = "Three"
+sayMe 4 = "Four"
+sayMe 5 = "Five"
+sayMe x = "Not between 1 and 5"
+
+factorial :: (Integral a) => a -> a
+factorial 0 = 1
+factorial n = n * factorial (n - 1)
+
+charName :: Char -> String
+charName 'a' = "Albert"
+charName 'b' = "Broseph"
+charName 'c' = "Cecil"
+
+addVectors :: (Num a) => (a, a) -> (a, a) -> (a, a)
+addVectors (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
+
+-- extract components of triples
+first :: (a, b, c) -> a
+first (x, _, _) = x
+
+second :: (a, b, c) -> b
+second (_, y, _) = y
+
+third :: (a, b, c) -> c
+third (_, _, z) = z
+
+head' :: [a] -> a
+head' [] = error "Can't call head on an empty list"
+head' (x:_) = x
+
+length' :: (Num b) => [a] -> b
+length' [] = 0
+length' (_:xs) = 1 + length' xs
+
+sum' :: (Num a) => [a] -> a
+sum' [] = 0
+sum' (x:xs) = x + sum' xs
+
+capital :: String -> String
+capital "" = "Empty string"
+capital all@(x:xs) = "The first letter of " ++ all ++ " is " ++ [x]
+
+--bmiTell :: (RealFloat a) => a -> String
+--bmiTell bmi
+--  | bmi <= 18.5 = "underweight"
+--  | bmi <= 25.0 = "normal"
+--  | bmi <= 30.0 = "overweight"
+--  | otherwise   = "Giant"
+
+bmiTell :: (RealFloat a) => a -> a -> String
+bmiTell weight height
+  | weight / height ^ 2 <= 18.5 = "underweight"
+  | weight / height ^ 2 <= 25.0 = "normal"
+  | weight / height ^ 2 <= 30.0 = "overweight"
+  | otherwise                   = "gigentic"
 
