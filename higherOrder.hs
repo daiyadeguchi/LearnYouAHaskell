5c7112038106fb3c8fefdb3f1c28923f700db335
daiyadeguchi <double.d.rugby@gmail.com>
2023-10-22 09:52:06 +0900

    maps and filters

--- higherOrder.hs
+++ higherOrder.hs
@@ -41,3 +41,13 @@ quicksort (x:xs) =
 largestDivisible :: (Integral a) => a
 largestDivisible = head (filter p [100000,99999..])
   where p x = x `mod` 3829 == 0
+
+chain :: (Integral a) => a -> [a]
+chain 1 = [1]
+chain n
+  | even n = n:chain (n `div` 2)
+  | odd n  = n:chain (n*3 + 1)
+
+numLongChains :: Int
+numLongChains = length (filter isLong (map chain [1..100]))
+  where isLong xs = length xs > 15
