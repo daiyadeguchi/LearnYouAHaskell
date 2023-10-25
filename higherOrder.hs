86abde94fc89306b3b8e583a7513a7793ae296e5
daiyadeguchi <double.d.rugby@gmail.com>
2023-10-26 07:23:08 +0900

    foldl

--- higherOrder.hs
+++ higherOrder.hs
@@ -15,7 +15,7 @@ applyTwice f x = f (f x)
 
 zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
 zipWith' _ [] _ = []
-zipWith' _ _ [] = []
+zipWith' _ _ [] = [] 
 zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
 
 -- lambda looks better when it comes to flip function
@@ -46,8 +46,7 @@ largestDivisible = head (filter p [100000,99999..])
 chain :: (Integral a) => a -> [a]
 chain 1 = [1]
 chain n
-  | even n = n:chain (n `div` 2)
-  | odd n  = n:chain (n*3 + 1)
+  | even n = n:chain (n `div` 2) | odd n  = n:chain (n*3 + 1)
 
 --numLongChains :: Int
 --numLongChains = length (filter isLong (map chain [1..100]))
@@ -58,8 +57,11 @@ numLongChains = length (filter (\xs -> length xs > 15) (map chain [1..100]))
 
 -- Both implementation is the same, but second one is not too good
 addThree :: (Num a) => a -> a -> a -> a
+
 --addThree x y z = x + y + z
 addThree = \x -> \y -> \z -> x + y + z
-
 flip' :: (a -> b -> c) -> b -> a -> c
 flip' f = \x y -> f y x
+
+sum' :: (Num a) => [a] -> a
+sum' xs = foldl (\acc x -> acc + x) 0 xs
